6c1982589cb6eafd8a0c584c42fb760f288b34ac
brainiac6 <giorgosbrainaic@hotmail.com>
2018-06-19 20:02:01 +0300

    antreass

--- src/main.java
+++ src/main.java
@@ -6,10 +6,11 @@ public class main {
         String le3i1 = "ioannou";
         String onomateponimo ;
 
+        for (int i=le3i.length();i<=0;i--)
+        System.out.println(le3i.codePointAt(6));
         onomateponimo = le3i1 + " " + le3i;
-        System.out.println(onomateponimo);
-        for (int i=le3i.length();i<=0;i--){
-            System.out.println(le3i);
+
+        {
 
 
 
