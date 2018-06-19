b9ef0c919420e59def0b4ace6406fbaf05fc7cfb
brainiac6 <giorgosbrainaic@hotmail.com>
2018-06-19 19:41:41 +0300

    antreas

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main {
+
+
+    public static void main(String[] args) {
+        String le3i = "antreas";
+        String le3i1 = "ioannou";
+        String onomateponimo ;
+
+        onomateponimo = le3i1 + " " + le3i;
+        System.out.println(onomateponimo);
+        for (int i=le3i.length();i<=0;i--){
+            System.out.println(le3i);
+
+
+
+        }
+    }
+
+}
